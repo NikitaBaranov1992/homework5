c3a4914496a59b9108db8e39f039c5cce6f86120
NikitaBaranov1992 <2486010@mail.ru>
2023-04-15 19:38:44 +0500

    add tast_score

---
+++ task_score.js
@@ -0,0 +1,11 @@
+let intro = 180;
+let git = 189;
+let js = 120;
+let nameFirst = "Никита";
+let nameLast = "Баранов";
+
+let avg = (intro + git + js) / 3;
+
+console.log(
+  `Средний балл за 3 модуля у студента ${nameFirst} ${nameLast} составляет: ${avg}`
+);
