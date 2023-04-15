7ed335a2309b98cb19fb032b37d6e470e13b3e74
NikitaBaranov1992 <2486010@mail.ru>
2023-04-15 20:17:30 +0500

    task 5.5

---
+++ file2.js
@@ -0,0 +1,10 @@
+const VALUE1 = 3;
+let value2 = Math.floor(Math.random() * 100);
+console.log(value2);
+
+let result = VALUE1 + value2 / 5 + 17;
+
+console.log(result);
+
+/*В данном случае при расчете переменной result идет приоритет согласно арифметическим правилам. 
+Сперва деление, потом сложения.JS*/
